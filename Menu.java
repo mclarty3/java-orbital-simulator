d5bfebfbc5b1796955c19d984a771ce325421862
Ryan <mclarty3@gmail.com>
2019-08-22 17:15:48 -0400

    Add function to resize menu components on resolution change

--- Menu.java
+++ Menu.java
@@ -6,6 +6,7 @@ public class Menu {
 
     public int bgColour;                                 // Background colour for the menu
     public List<Button> menuButtons = new ArrayList<>(); // List of buttons to be displayed in the menu
+    public List<MenuText> menuTexts = new ArrayList<>(); // List of texts to be displayed in the menu
     public Menu menuToReturnTo;                          // Menu to display (if any) when this menu is exited
 
     public boolean isDisplayed = false;
@@ -19,10 +20,10 @@ public class Menu {
     // If the mouse is on a button, it highlights it. Otherwise, it leaves it un-highlighted
     public void checkButtonHighlights(int x, int y) {
         for (Button button: menuButtons) {
-            if (button.isHighlighted() && !button.mouseOnButton(x, y)) {
+            if (button.highlighted && !button.mouseOnButton(x, y)) {
                 button.unHighlightColour();
             }
-            else if (!button.isHighlighted() && button.mouseOnButton(x, y)) {
+            else if (!button.highlighted && button.mouseOnButton(x, y)) {
                 button.highlightColour();
             }
         }
@@ -32,5 +33,27 @@ public class Menu {
         menuButtons.addAll(Arrays.asList(buttons));
     }
 
+    public void addTexts(MenuText... texts) {
+        menuTexts.addAll(Arrays.asList(texts));
+    }
 
+    public void resizeComponents(int oldWidth, int oldHeight, int newWidth, int newHeight) {
+        float widthRatio = (float) newWidth / (float) oldWidth;
+        float heightRatio = (float) newHeight / (float) oldHeight;
+        //System.out.println(newWidth + " / " + oldWidth + ": " + widthRatio + " " + newHeight + " / " + oldHeight + ": " + heightRatio);
+        for (Button button: menuButtons) {
+            //System.out.println("Old button (" + button.xPos + ", " + button.yPos + " W: " + button.width + " H: " + button.height);
+            button.xPos *= widthRatio;
+            button.width *= widthRatio;
+            button.yPos *= heightRatio;
+            button.height *= heightRatio;
+            //System.out.println("New button (" + button.xPos + ", " + button.yPos + " W: " + button.width + " H: " + button.height);
+        }
+        for (MenuText text: menuTexts) {
+            text.xPos *= widthRatio;
+            text.width *= widthRatio;
+            text.yPos *= heightRatio;
+            text.height *= heightRatio;
+        }
+    }
 }
