80d6f5ad58002260e3dbc92bc96a6fb410c44daa
Ryan <mclarty3@gmail.com>
2019-08-23 14:25:43 -0400

    Change planet radii to accurate values

--- main/java/Main.java
+++ main/java/Main.java
@@ -59,10 +59,10 @@ public class Main {
         Body sun = new Body("Sun", 1.989e30, 0, 0, 0, 0, 695.51e6, 0xFFECD67E);
         Body mercury = new Body("Mercury", 0.330e24, -57.9e9, 0, 0, -47.4e3, 2439, 0xFFFFFF00);
         Body venus = new Body("Venus", 4.87e24, -108.2e9, 0, 0, -35e3, 6057, 0xFF8BB7AB);
-        Body earth = new Body("Earth", 5.97e24, 1 * AU, 0, 0, 29.8e3, 60.371e6, 0xFF8CB1DE);
+        Body earth = new Body("Earth", 5.97e24, 1 * AU, 0, 0, 29.8e3, 6378, 0xFF8CB1DE);
         //Body moon = new Body();
-        Body mars = new Body("Mars", 0.642e24, -227.9e9, 0, 0, -24.1e3, 30.381e6, 0xFFFF0000);
-        Body jupiter = new Body("Jupiter", 1898e24, 778.6e9, 0, 0, 13.1e3, 71492e3, 0xFFA79C86);
+        Body mars = new Body("Mars", 0.642e24, -227.9e9, 0, 0, -24.1e3, 3396, 0xFFFF0000);
+        Body jupiter = new Body("Jupiter", 1898e24, 778.6e9, 0, 0, 13.1e3, 71492, 0xFFA79C86);
         Body saturn = new Body("Saturn", 568e24, -1433.5e9, 0, 0, -9.7e3, 60268, 0xFF343E47);
         Body uranus = new Body("Uranus", 86.8e24, 2872.5e9, 0, 0, 6.8e3, 25509, 0xFFD5FBFC);
         Body neptune = new Body("Neptune", 102e24, 0, 4495.1e9, -5.4e3, 0, 24764, 0xFF3e54E8);
